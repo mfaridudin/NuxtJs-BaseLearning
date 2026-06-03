db422a530459b1ebbbff4797cb2e9407754bfa52
Muhammad Farid <hi.faridudin@gmail.com>
2026-06-03 19:43:42 +0700

    feat: belajar state dengan ref()

--- app/pages/state/index.vue
+++ app/pages/state/index.vue
@@ -1,17 +1,31 @@
 <template>
     <div class="container">
-        Counter: {{ counter }}
-        <button @click="counter++">
-            +
-        </button>
-        <button @click="counter--">
-            -
-        </button>
+        <div>
+            Counter dengan useState: {{ counter }}
+            <button @click="counter++">
+                +
+            </button>
+            <button @click="counter--">
+                -
+            </button>
+        </div>
+
+        <div>
+            Counter dengan ref: {{ count }}
+            <button @click="count++">
+                +
+            </button>
+            <button @click="count--">
+                -
+            </button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 const counter = useState('counter', () => Math.round(Math.random() * 1000))
+
+const count = ref(0)
 </script>
 
 <style scoped>
